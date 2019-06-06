5c39a669f39cc0fde13810a8e668aa6568230c42
dinhooliveira <oliveiracienciacomputacao@gmail.com>
2019-06-05 22:43:50 -0300

    novo

--- src/Servidor1.java
+++ src/Servidor1.java
@@ -2,7 +2,7 @@ import java.rmi.Naming;
 
 public class Servidor1 {
 
-	Servidor1() {
+	public static void main(String[] args) {
 		try {
 			Implementacao Servidor = new Implementacao();
 			Naming.rebind("//192.168.0.104/rmi", Servidor);
@@ -12,10 +12,6 @@ public class Servidor1 {
 			e.printStackTrace();
 			System.exit(2);
 		}
-	}
-
-	public static void main(String[] args) {
-		new Servidor1();
 		System.out.println("Aguardando inicialização do objeto...");
 	}
 }
